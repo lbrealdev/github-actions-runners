44acbabe7261f0617c886616dd41e1e55230c804
lbrealdev <lbrealdeveloper@gmail.com>
2022-12-22 18:41:34 +0100

    fix

--- cleanup-runner-workflow.sh
+++ cleanup-runner-workflow.sh
@@ -3,7 +3,7 @@
 # Set the pipefail
 set -o pipefail
 
-# Variable to the path of the workflow directory.
+# Set the variable to the workflow directory path.
 GITHUB_ACTIONS_WORKFLOW_DIR="/home/ec2-user/actions-runner/_work/"
 
 function cleanup_github_actions_workflow_dir() {
