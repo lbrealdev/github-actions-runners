9a2e1cc661495b19b26734c133f24e68e3d872a1
lbrealdev <lbrealdeveloper@gmail.com>
2022-12-28 21:01:59 +0100

    feat: add verify variable and other stuffs

--- watchdocker.sh
+++ watchdocker.sh
@@ -2,16 +2,27 @@
 
 set -o pipefail
 
+# Get all containers by .ID
 function get_container_id() {
-  docker_ids=$(docker ps -a --format '{{.ID}}')
+  docker_container_ids=$(docker ps -a --format '{{.ID}}')
 }
 
-# Delete all containers from this output.
-if [ -n "$docker_ids" ]; then
-  for container in $docker_ids; do
-    echo "$container"
-  done
-else
-  echo "Nof found!"
-fi
+function remove_container() {
+  local container_id=$1
+  docker rm -f "$container_id"
+}
 
+function main() {
+  get_container_id
+  # Remove all containers.
+  if [ -n "$docker_container_ids" ]; then
+    for container_id in $docker_container_ids; do
+      echo "$container_id"
+      # remove_container "$container_id"
+    done
+  else
+    echo "No containers found!"
+  fi
+}
+
+main
