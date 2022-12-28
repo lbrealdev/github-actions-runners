9fb4593a0c9a28f55456cc56c901c11c8dee8a75
lbrealdev <lbrealdeveloper@gmail.com>
2022-12-28 20:46:05 +0100

    feat: add new function

--- watchdocker.sh
+++ watchdocker.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+set -o pipefail
+
+function get_container_id() {
+  docker_ids=$(docker ps -a --format '{{.ID}}')
+}
+
 # Delete all containers from this output.
-for container in $(docker ps -a --format '{{.ID}}'); do
-  echo "$container"
-done
+if [ -n "$docker_ids" ]; then
+  for container in $docker_ids; do
+    echo "$container"
+  done
+else
+  echo "Nof found!"
+fi
+
